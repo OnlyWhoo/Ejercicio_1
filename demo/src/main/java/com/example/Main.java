e87c7ced2c62548cfb88e68ec7c302c020fa868e
OnlyWhoo <jdavorgor@gmail.com>
2026-02-27 22:43:07 -0500

    Creación de la clase Main

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,6 +2,16 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Persona persona1 = new Persona("Samuel", "Rengifo", "1457893621", 1998);
+        Persona persona2 = new Persona("Jhon", "Montes", "1572364894", 2001);
+
+        System.out.println("Información de la persona 1:");
+        persona1.imprimirInformacion();
+        System.out.println("Edad: " +persona1.calcularEdad()+ " años");
+
+        System.out.println("\nInformación de la persona 2:");
+        persona2.imprimirInformacion();
+        System.out.println("Edad: " +persona2.calcularEdad()+ " años");
     }
 }
